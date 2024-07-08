3453434c1eff37f12ab47b07e10ad1847097c485
theworstkhoi <kylekhoitv@gmail.com>
2024-07-08 09:40:50 +0700

    fix: base url

--- src/Header.jsx
+++ src/Header.jsx
@@ -5,27 +5,27 @@ import ReactLogo from "./assets/react.svg"
 export default function Header() {
     return (
         <div className="header">
-            <Link to="/" className="logo">
+            <Link to={`${import.meta.env.BASE_URL}/`} className="logo">
                 <img src={ReactLogo} alt="Logo" />
             </Link>
             <ul className="page-list">
                 <li>
-                    <Link className="page-button" to="/vocab">
+                    <Link className="page-button" to={`${import.meta.env.BASE_URL}/vocab`}>
                         VocabLearning
                     </Link>
                 </li>
                 <li>
-                    <Link className="page-button" to="/rubiks">
+                    <Link className="page-button" to={`${import.meta.env.BASE_URL}/rubiks`}>
                         RubiksTimor
                     </Link>
                 </li>
                 <li>
-                    <Link className="page-button" to="/blogs">
+                    <Link className="page-button" to={`${import.meta.env.BASE_URL}/blogs`}>
                         Blogs
                     </Link>    
                 </li>
                 <li>
-                    <Link className="page-button" to="/about">
+                    <Link className="page-button" to={`${import.meta.env.BASE_URL}/about`}>
                         About
                     </Link>
                 </li>
