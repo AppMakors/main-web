5439032f3023bae9a19560ca520ca14106029159
Vo Tran Phi <songokupkj@gmail.com>
2024-07-13 09:40:42 +0700

    feat: icon using svg

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,6 +1,10 @@
 import "./styles/Header.css"
 import { Link } from "react-router-dom"
 import ReactLogo from "./assets/react.svg"
+import AboutIcon from "./assets/icon_about.svg"
+import BlogIcon from "./assets/icon_blog.svg"
+import LanguagueIcon from "./assets/icon_language.svg"
+import RubikIcon from "./assets/icon_rubik.svg"
  
 export default function Header() {
     return (
@@ -10,23 +14,23 @@ export default function Header() {
             </Link>
             <ul className="page-list">
                 <li>
-                    <Link className="page-button" to={`${import.meta.env.BASE_URL}vocab`}>
-                        VocabLearning
+                    <Link to={`${import.meta.env.BASE_URL}vocab`}>
+                        <img className="page-button" src={LanguagueIcon} alt="Logo"></img>
                     </Link>
                 </li>
                 <li>
-                    <Link className="page-button" to={`${import.meta.env.BASE_URL}rubiks`}>
-                        RubiksTimor
+                    <Link to={`${import.meta.env.BASE_URL}rubiks`}>
+                        <img className="page-button" src={RubikIcon} alt="Logo"></img>
                     </Link>
                 </li>
                 <li>
-                    <Link className="page-button" to={`${import.meta.env.BASE_URL}blogs`}>
-                        Blogs
+                    <Link to={`${import.meta.env.BASE_URL}blogs`}>
+                        <img className="page-button" src={BlogIcon} alt="Logo"></img>
                     </Link>    
                 </li>
                 <li>
-                    <Link className="page-button" to={`${import.meta.env.BASE_URL}about`}>
-                        About
+                    <Link to={`${import.meta.env.BASE_URL}about`}>
+                        <img className="page-button" src={AboutIcon} alt="Logo"></img>
                     </Link>
                 </li>
             </ul>
