c39ada65615bbfc441042eb8dad723adce7191e1
Vo Tran Phi <songokupkj@gmail.com>
2024-07-19 17:03:24 +0700

    feat: scramble and scramble's image generator

--- src/pages/RubiksTimor.jsx
+++ src/pages/RubiksTimor.jsx
@@ -2,21 +2,21 @@ import "../assets/style.css"
 import "../styles/RubiksTimor.css"
 import {useState, useEffect} from 'react'
 
+var cstimerWorker=(function(){var worker=new Worker('node_modules/cstimer_module/cstimer_module.js');var callbacks={};var msgid=0;worker.onmessage=function(e){var data=e.data;var callback=callbacks[data[0]];delete callbacks[data[0]];callback&&callback(data[2])}
+function callWorkerAsync(type,details){return new Promise(function(type,details,resolve){++msgid;callbacks[msgid]=resolve;worker.postMessage([msgid,type,details])}.bind(null,type,details))}
+return{getScrambleTypes:function(){return callWorkerAsync('scrtype')},getScramble:function(){return callWorkerAsync('scramble',Array.prototype.slice.apply(arguments))},setSeed:function(seed){return callWorkerAsync('seed',[seed])},setGlobal:function(key,value){return callWorkerAsync('set',[key,value])},getImage:function(scramble,type){return callWorkerAsync('image',[scramble,type])}}})()
+
 export default function RubiksTimor() {
     const [solves, setSolves] = useState([]);
 
-    const addSolve = (solve) => {
-        
-    }
-
     return <div className="rubiks-main" onClick={console.log("Load")}>
         <div className="left-panel">
-            <ul className="solve-list">
-
-            </ul>
+            <ul className="solve-list"></ul>
         </div>
+
         <div className="time-container">
-            <div className="scramble">Scramble</div>
+            <Scramble />
+
             <div className="timer-wrapper">
                 <Timer />
                 <div className="ao5">
@@ -28,6 +28,71 @@ export default function RubiksTimor() {
             </div>
         </div>
     </div>
+}
+
+function Scramble({ }) {
+    const [type, setType] = useState(0);
+
+    var wca_events = [
+        ["3x3x3", "333", 0],
+        ["2x2x2", "222so", 0],
+        ["4x4x4", "444wca", 0],
+        ["5x5x5", "555wca", 60],
+        ["6x6x6", "666wca", 80],
+        ["7x7x7", "777wca", 100],
+        ["3x3 bld", "333ni", 0],
+        ["3x3 fm", "333fm", 0],
+        ["3x3 oh", "333", 0],
+        ["clock", "clkwca", 0],
+        ["megaminx", "mgmp", 70],
+        ["pyraminx", "pyrso", 10],
+        ["skewb", "skbso", 0],
+        ["sq1", "sqrs", 0],
+        ["4x4 bld", "444bld", 40],
+        ["5x5 bld", "555bld", 60],
+        ["3x3 mbld", "r3ni", 5]
+    ];
+
+    const [scramble, setScramble] = useState("");
+    const [scrambleSvg, setScrambleSvg] = useState("");
+
+    useEffect(() => {
+        async function hehe() {
+            const scrRes = await cstimerWorker.getScramble(wca_events[type][1], wca_events[type][2]);
+            setScramble(await scrRes);
+            const svgRes = await cstimerWorker.getImage(scrRes, wca_events[type][1]);
+            setScrambleSvg(await svgRes);
+        }
+
+        hehe();
+    }, [type])
+
+    return <div>
+        <ScrambleSettings setType={setType} wca_events={wca_events} />
+        <ScrambleText scramble={scramble} />
+        <ScrambleImage scrambleSvg={scrambleSvg} />
+    </div>
+}
+
+function ScrambleSettings({ setType, wca_events }) {
+    return <div className="scramble-settings">
+        <select onChange={(e) => {setType(e.target.value)}}>
+            {wca_events.map((v, i) => <option key={`type${i}`} value={i}>{v[0]}</option>)}
+        </select>
+
+        <button onClick={(e) => {}}>left</button>
+
+        <button onClick={(e) => {}}>right</button>
+    </div>
+}
+
+function ScrambleText({ scramble }) {
+    console.log(scramble);
+    return <div className="scramble">{scramble}</div>;
+}
+
+function ScrambleImage({ scrambleSvg }) {
+    return <div className="scramble-image" dangerouslySetInnerHTML={{ __html: scrambleSvg }}></div>
 }
 
 function Timer({ }) {
