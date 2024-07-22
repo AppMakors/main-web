0ccd9a9f63094538a6d765762d2fd15e58ab1a47
Vo Tran Phi <songokupkj@gmail.com>
2024-07-22 14:49:06 +0700

    feat: new 3d cube loader component

---
+++ src/components/global/CubeLoader.jsx
@@ -0,0 +1,53 @@
+import "./CubeLoader.css"
+
+export default function CubeLoader() {
+    return <div class="scene">
+        <div class="shadow"></div>
+        <div class="jumper">
+            <div class="spinner">
+                <div class="scaler">
+                    <div class="loader">
+                        <div class="cuboid">
+                            <div class="cuboid__side">
+                                <div class="line-1"></div>
+                                <div class="line-2"></div>
+                                <div class="line-3"></div>
+                                <div class="line-4"></div>
+                            </div>
+                            <div class="cuboid__side">
+                                <div class="line-1"></div>
+                                <div class="line-2"></div>
+                                <div class="line-3"></div>
+                                <div class="line-4"></div>
+                            </div>
+                            <div class="cuboid__side">
+                                <div class="line-1"></div>
+                                <div class="line-2"></div>
+                                <div class="line-3"></div>
+                                <div class="line-4"></div>
+                            </div>
+                            <div class="cuboid__side">
+                                <div class="line-1"></div>
+                                <div class="line-2"></div>
+                                <div class="line-3"></div>
+                                <div class="line-4"></div>
+                            </div>
+                            <div class="cuboid__side">
+                                <div class="line-1"></div>
+                                <div class="line-2"></div>
+                                <div class="line-3"></div>
+                                <div class="line-4"></div>
+                            </div>
+                            <div class="cuboid__side">
+                                <div class="line-1"></div>
+                                <div class="line-2"></div>
+                                <div class="line-3"></div>
+                                <div class="line-4"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+}
