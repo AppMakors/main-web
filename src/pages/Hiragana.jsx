858f83fcd6a5ecb3f391ef862a10deccc034a2a6
Vo Tran Phi <songokupkj@gmail.com>
2024-07-22 23:58:40 +0700

    fix: answer's usually in position 1 or 2

--- src/pages/Hiragana.jsx
+++ src/pages/Hiragana.jsx
@@ -3,23 +3,25 @@ import "../styles/Hiragana.css"
 import CubeLoader from "../components/global/CubeLoader.jsx"
 
 export default function Hiragana() {
+    const NUMBER_OF_CHOICES = 5;
+
     const [letters, setLetters] = useState([]);
     const [signal, setSignal] = useState(false);
 
     useEffect(() => {
         async function getLetters() {
-			const response = await fetch(`${location.origin}/main-web/linguistics/hiragana/alphabet.json`);
-			const responseList = await response.json();
+            const response = await fetch(`${location.origin}/main-web/linguistics/hiragana/alphabet.json`);
+            const responseList = await response.json();
 
             await new Promise(resolve => setTimeout(resolve, 2000));
 
             setLetters(responseList);
-		}
+        }
 
-		getLetters();
+        getLetters();
     }, [])
 
-    if (!letters.length) 
+    if (!letters.length)
         return <CubeLoader />;
 
     const letterIdx = myRandom(0, letters.length - 1);
@@ -33,20 +35,22 @@ export default function Hiragana() {
         letters[letterIdx][isRomanji ^ 1]
     ];
 
-    while (choices.length < 4) {
+    while (choices.length < NUMBER_OF_CHOICES) {
         const randInt = myRandom(0, letters.length - 1);
 
         if (selectedIdx.indexOf(randInt) === -1) {
-            randInt % 2 ? choices.push(letters[randInt][isRomanji ^ 1]) : choices = [letters[randInt][isRomanji ^ 1], ...choices];
+            choices.push(letters[randInt][isRomanji ^ 1]);
             selectedIdx.push(randInt);
         }
     }
+
+    shuffle(choices);
 
     const choiceHandler = (e) => {
         if (e.target.value !== letters[letterIdx][isRomanji ^ 1]) {
             e.target.classList.add("wrong-ans");
             e.target.classList.remove("ans");
-            const id = setTimeout(() => {e.target.classList.remove("wrong-ans"); e.target.classList.add("ans"); clearTimeout(id);}, 300);
+            const id = setTimeout(() => { e.target.classList.remove("wrong-ans"); e.target.classList.add("ans"); clearTimeout(id); }, 300);
         } else {
             e.target.classList.remove("ans");
             e.target.classList.add("right-ans");
@@ -55,17 +59,32 @@ export default function Hiragana() {
         }
     };
 
-	return (
-		<div className="hiragana-main">
+    return (
+        <div className="hiragana-main">
             <p>{letter}</p>
 
             <div className="choice-row">
                 {choices.map((v, i) => <button className="ans" key={new Date().getTime() + i} value={v} onClick={choiceHandler}>{v}</button>)}
             </div>
         </div>
-	)
+    )
 }
 
 function myRandom(min, max) {
     return parseInt(new Date().getTime() % (max - min + 1) + min);
+}
+
+// Fisher–Yates Shuffle Algorithm: https://bost.ocks.org/mike/shuffle/
+function shuffle(array) {
+    var m = array.length, t, i;
+
+    while (m) {
+        i = Math.floor(Math.random() * m--);
+
+        t = array[m];
+        array[m] = array[i];
+        array[i] = t;
+    }
+
+    return array;
 }
