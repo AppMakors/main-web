9c34a04fd06b9bf93d9a08796d22f160e31d388b
theworstkhoi <kylekhoitv@gmail.com>
2024-07-24 23:32:51 +0700

    feat: local save for solves

--- src/pages/RubiksTimor.jsx
+++ src/pages/RubiksTimor.jsx
@@ -2,28 +2,21 @@ import "../assets/style.css";
 import "../styles/RubiksTimor.css";
 import {useState, useEffect} from 'react';
 
-const wca_events = [
-    ["3x3x3", "333", 0],
-    ["2x2x2", "222so", 0],
-    ["4x4x4", "444wca", 0],
-    ["5x5x5", "555wca", 60],
-    ["6x6x6", "666wca", 80],
-    ["7x7x7", "777wca", 100],
-    ["3x3 bld", "333ni", 0],
-    ["3x3 fm", "333fm", 0],
-    ["3x3 oh", "333", 0],
-    ["clock", "clkwca", 0],
-    ["megaminx", "mgmp", 70],
-    ["pyraminx", "pyrso", 10],
-    ["skewb", "skbso", 0],
-    ["sq1", "sqrs", 0],
-    ["4x4 bld", "444bld", 40],
-    ["5x5 bld", "555bld", 60],
-    ["3x3 mbld", "r3ni", 5]
-];
-
 export default function RubiksTimor() {
     const [solves, setSolves] = useState([]);
+
+    useEffect(() => {
+        const localSolvesString = localStorage.getItem("session1");
+        if (!localSolvesString) {
+            localStorage.setItem("session1", "[]");
+        } else {
+            setSolves(JSON.parse(localSolvesString));
+        }
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem("session1", JSON.stringify(solves));
+    }, [solves])
     
     return <div className="rubiks-main">
         <div className="left-panel">
@@ -44,6 +37,26 @@ function Scramble({ solveTime, setScrambleAndType }) {
     const [scrambleSvg, setScrambleSvg] = useState("");
     const [renewSignal, setRenewSignal] = useState(false);
 
+    const wca_events = [
+        ["3x3x3", "333", 0],
+        ["2x2x2", "222so", 0],
+        ["4x4x4", "444wca", 0],
+        ["5x5x5", "555wca", 60],
+        ["6x6x6", "666wca", 80],
+        ["7x7x7", "777wca", 100],
+        ["3x3 bld", "333ni", 0],
+        ["3x3 fm", "333fm", 0],
+        ["3x3 oh", "333", 0],
+        ["clock", "clkwca", 0],
+        ["megaminx", "mgmp", 70],
+        ["pyraminx", "pyrso", 10],
+        ["skewb", "skbso", 0],
+        ["sq1", "sqrs", 0],
+        ["4x4 bld", "444bld", 40],
+        ["5x5 bld", "555bld", 60],
+        ["3x3 mbld", "r3ni", 5]
+    ];
+
     // listen to the changes of:
     // 1. type (user selects scramble's type)
     // 2. signal (renew signal from Timer's stop handler)
@@ -58,7 +71,7 @@ function Scramble({ solveTime, setScrambleAndType }) {
 
             setScrambleAndType({
                 scramble: scrRes,
-                type: type
+                type: wca_events[type][0]
             });
         }
 
@@ -209,6 +222,7 @@ function TimeContainer({ setSolves }) {
                 scramble: scrambleAndType.scramble,
                 type: scrambleAndType.type,
             };
+            
             setSolves((oldSolves) => [...oldSolves, newSolve]);
         }
     }, [solveTime]);
@@ -259,7 +273,7 @@ function SolveItem({ index, solve }) {
     return <>
         {hoverObject.isHover && 
         <div className="solve-info-card" style={{ "left": `${hoverObject.leftOffset}px`, "top": `${hoverObject.topOffset}px`}}>
-            <p>{wca_events[solve.type][0]}</p>
+            <p>{solve.type}</p>
             <p>{solve.scramble}</p>
         </div>}
         <li className="solve" onMouseEnter={mouseEnterHandler} onMouseLeave={mouseLeaveHandler}>
