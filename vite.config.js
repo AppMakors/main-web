9e9055a0ed22f7d9c89d28917549ead88ca31a29
Vo Tran Phi <songokupkj@gmail.com>
2024-11-06 15:38:52 +0700

    fix: deployment things

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/main-web/" 
+  base: "/main-web/",
+  server: {
+    host: true, // or use '0.0.0.0' to listen on all network interfaces
+    port: 5173, // Optional: specify the port, default is 5173
+  },
 })
